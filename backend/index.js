577351e26ad46f2b1362a72388585540c86a317a
GSylph <sheranidanish12@gmail.com>
2024-12-22 00:47:16 +0530

    Added exception handling for confirming all book schema inputs

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,21 @@ app.get('/',(request,response)=>{
     return response.status(234).send('Welcome to MERN stack tutorial');
 });
 
+app.post('/books',async (request,response)=>{
+    try{
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ){return response.status(400).send({
+            message:'Send all required fields: title, author, publishYear', 
+        });}
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 
 mongoose
